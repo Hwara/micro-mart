c3e541e9851f27c08692fe3a4f972ecf2d6b8444
JinYeong Park <hwara1221@gmail.com>
2026-05-18 20:31:24 +0900

    chore: 스크립트 빌드 실패 시 종료 feat/#12

--- scripts/image_build_push.bat
+++ scripts/image_build_push.bat
@@ -7,6 +7,11 @@ set TAG=local
 cd /d "%~dp0.."
 
 docker compose -f docker/services.yaml build
+if errorlevel 1 (
+    echo Build failed. Skipping push.
+    exit /b 1
+)
+
 docker compose -f docker/services.yaml push
 
 endlocal
